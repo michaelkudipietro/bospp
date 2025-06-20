2afb58c2d41ad0cfdeb9cfbba99b86bfa5355b28
michaelkudipietro <56523397+michaelkudipietro@users.noreply.github.com>
2025-06-19 22:38:29 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -117,3 +117,32 @@ def main():
 
                 input('Press Enter to continue...')
                 print('\n\n')
+
+                def getBet(maxBet):
+                    '''Ask the player how much they want to bet for this round.'''
+                    while True:
+                        print('How much do you bet? (1-{}, or QUIT)'.format(maxBet))
+                        bet = input('> ').upper().strip()
+                        if bet =='QUIT':
+                            print('Thanks for playing!')
+                            sys.exit()
+                        if not bet.isdecimal():
+                            continue #if player didn't enter a number, ask again.
+
+                        bet=int(bet)
+                        if 1 <= bet <= maxBet:
+                            return bet #Player entered a valid bet
+            def getDeck():
+                '''Return a list of (rank,suit) tuples for all 52 cards'''
+                deck = []
+                for suit in (HEARTS,DIAMONDS, SPADES, CLUBS)
+                    for rank in range(2,11):
+                        deck.append((str(rank),suit))#Add the numbered cards.
+                    for rank in ('J','Q','K','A'):
+                        deck.append((rank,suit)) #Add the numbered cards
+                random.shuffle(deck)
+                return deck
+
+            def displayHands(playerHand,dealerHand,showDealerHand):
+                '''Show the player's and dealers cards. Hide the dealer's first card if showDealerHand is False'''
+                #START HERE
